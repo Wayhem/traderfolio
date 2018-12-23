0c39b3918a8a7073e76a39da287e50e907b451bc
Jose Villarroel <oscarclarke9900@gmail.com>
2018-12-23 19:20:01 -0400

    finished modal looks so nice, had to add a plugin to react in order to use class based components methods holy molly never setting react from scratch again, better use create-react-app, also had to setup webpack to be able to use svg, another downside for not having create-react-app start your project, so god styling is annoying if your css skills are ok, I really gotta learn that too but one thing at a time, so modal for funds adding is ready, soon enough I will start handling data and comparing it to API.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Blockchain from 'svg-react-loader?name=Blockchain!../images/blockchain.svg';
+import './Modal.css';
 
-export default function Modal(props) {
-    return <div className="bg-modal"> 
-        <div className="modal-content">
-            <Blockchain />
+class Modal extends Component {
 
-            <form>
-                <input type="text" placeholder="Ticker symbol"></input>
-                <input type="number" placeholder="Amount"></input>
-            </form>
+    closeModal = () => {
+        document.querySelector('.bg-modal').style.display = 'none';
+        let inputs = Array.from(document.querySelectorAll('.modal-input'));
+        inputs.forEach(el => el.value = '');
+    }
+
+    render() {
+        return <div className="bg-modal"> 
+            <div className="modal-content">
+                <div className="close" onClick={this.closeModal}>+</div>
+                <Blockchain />
+                <form className="modal-form">
+                    <input id="input" className="modal-input Input-text" type="text" placeholder="BTC, ETH, or.."></input>
+                    <label htmlFor="input" className="Input-label">Ticker</label>
+                    <input id="input2" className="modal-input Input-text" type="text" placeholder="Amount"></input>
+                    <label htmlFor="input2" className="Input-label2">Amount</label>
+                    <a href="#" id="addButton" className="btn btn-primary badge-pill">Submit</a>
+                </form>
+            </div>
         </div>
-    </div>
+    }
 }
+
+export default Modal;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,38 +1,44 @@
 import React, { Component } from "react";
 
 class Sidebar extends Component{
-  render(){
-    return(
-      <nav id="sidebar">
-            <div className="sidebar-header">
-                <h3 className="brand pt-5">Traderfolio</h3>
-            </div>
-    
-            <ul className="list-unstyled components">
-                <li>
-                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Menu</a>
-                    <ul className="collapse list-unstyled" id="homeSubmenu">
-                        <li>
-                            <a href="#">Option 1</a>
-                        </li>
-                        <li>
-                            <a href="#">Option 2</a>
-                        </li>
-                        <li>
-                            <a href="#">Option 3</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="https://jvilladev.com">Portfolio</a>
-                </li>
-                <li>
-                    <a href="https://jvilladev.com/#contact">Contact</a>
-                </li>
-            </ul>
-        </nav>    
-    );
-  }
+
+    ShowModal = (e) => {
+        e.preventDefault();
+        document.querySelector('.bg-modal').style.display = 'flex';
+    }
+
+    render(){
+        return(
+        <nav id="sidebar">
+                <div className="sidebar-header">
+                    <h3 className="brand pt-5">Traderfolio</h3>
+                </div>
+        
+                <ul className="list-unstyled components">
+                    <li>
+                        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Menu</a>
+                        <ul className="collapse list-unstyled" id="homeSubmenu">
+                            <li>
+                                <a onClick={this.ShowModal} href="">Add Funds</a>
+                            </li>
+                            <li>
+                                <a href="#">Option 2</a>
+                            </li>
+                            <li>
+                                <a href="#">Option 3</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="https://jvilladev.com">Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="https://jvilladev.com/#contact">Contact</a>
+                    </li>
+                </ul>
+            </nav>    
+        );
+    }
 }
 
 export default Sidebar;
